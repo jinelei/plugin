f383e9bc23b0e85962ed06e67d01f3a320ce2aee
jinelei <jinelei@163.com>
2023-07-04 20:29:15 +0800

    使用Bundle资源

--- src/main/kotlin/com/github/jinelei/plugin/actions/AnalysisEntity.java
+++ src/main/kotlin/com/github/jinelei/plugin/actions/AnalysisEntity.java
@@ -2,6 +2,9 @@ package com.github.jinelei.plugin.actions;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @Author: jinelei
@@ -12,7 +15,16 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 public class AnalysisEntity extends AnAction {
 
     @Override
-    public void actionPerformed(AnActionEvent e) {
-        System.out.println(e);
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        e.getPresentation().setEnabled(true);
+        update(e);
+    }
+
+    @Override
+    public void update(@NotNull AnActionEvent e) {
+        // 获取当前选中的文件
+        VirtualFile file = e.getData(CommonDataKeys.VIRTUAL_FILE);
+        // 如果选中的是 Java 文件，则启用 Action，否则禁用 Action
+        e.getPresentation().setEnabled(file != null && "java".equals(file.getExtension()));
     }
 }
